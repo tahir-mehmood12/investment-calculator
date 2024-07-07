976a0cccfe642ec42494be6f47f85cfc46d5ea47
Tahir Mehmood <126563708+tahir-mehmood12@users.noreply.github.com>
2024-07-07 15:09:29 +0500

    complete the investment calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const validInput = userInput.duration > 0;
+
+  function handleChangeInput(identifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [identifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChangeInput} userInput={userInput} />
+      {!validInput && (
+        <p className="center">Please Enter a duration greater then zero</p>
+      )}
+      {validInput && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
